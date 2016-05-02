9a5608099f6fa44ebc229e77249f56fb19aa3a13
zhongwei <1zhongwei@tongji.edu.cn>
2016-05-02 14:54:45 +0800

    加了注释！

--- Sources/steer.c
+++ Sources/steer.c
@@ -74,11 +74,11 @@ void SteerControl(void)
 	{
 		return;
 	}
-	if(wrong_flag==1)
+	if(wrong_flag==1)		
 	{
 		close_supersonic=1;//触发关闭超声波标志
 		wrong_count++;
-		if(success)
+		if(success)		//判断到灯塔边上
 		{
 			targetspeed=cyclespeed;
 			BEE = 1;
@@ -91,14 +91,14 @@ void SteerControl(void)
 		}
 		else
 		{
-			if(wrong_count==1)
+			if(wrong_count==1)	//1次没看到灯塔，保持舵机值
 			{
 				Steer_PWM[3]=(Steer_PWM[2]+Steer_PWM[1])/2;
 				SET_steer(Steer_PWM[3]);
 				Steer_PWM[0]=Steer_PWM[1];Steer_PWM[1]=Steer_PWM[2];Steer_PWM[2]=Steer_PWM[3];
 				return;
 			}
-			else if(wrong_count>=2)
+			else if(wrong_count>=2)	//2次没看到灯塔，向左打足转圈
 			{
 				targetspeed=cyclespeed;
 				BEE = 1;
@@ -114,14 +114,14 @@ void SteerControl(void)
 	{
 		wrong_count=0;
 		BEE=0;
-		if(pix_i<31)
+		if(pix_i<31)	
 		{
-			if(pix_i<31)
+			if(pix_i<31)		//在远处，现在超声全关了，所以close_supersonic=1;，正常close_supersonic=0；远处开超声
 			{
-					close_supersonic=1;
-					targetspeed=straightspeed;
+				close_supersonic=1;
+				targetspeed=straightspeed;
 			}
-			else
+			else				//在近处，现在超声全关了
 			{  
 				close_supersonic=1;//触发关闭超声波标志
 				targetspeed=straightspeed;
@@ -142,7 +142,7 @@ void SteerControl(void)
 			{
 				success=1;
 			}
-			if(pix_i>55&&pix_j<cycle_j)
+			if(pix_i>55&&pix_j<cycle_j)		//靠近灯塔，位置符合灯塔在左下角条件，开始转向
 			{
 				targetspeed=cyclespeed;
 				BEE = 1;
@@ -152,7 +152,7 @@ void SteerControl(void)
 				Steer_PWM[0]=Steer_PWM[1];Steer_PWM[1]=Steer_PWM[2];Steer_PWM[2]=Steer_PWM[3];
 				return;
 			}
-			else
+			else		//靠近灯塔，但位置不符合灯塔在左下角条件，稍微跑偏一点预转向
 			{
 				targetspeed=cyclespeed;
 				//小车离灯塔较近时为了使小车不直接朝灯塔跑，将目标值46进行修正如下
@@ -171,7 +171,7 @@ void SteerControl(void)
 		}
 	}	
 }
-byte BarrierJudge(void)
+byte BarrierJudge(void)	//超声优先级
 {
 	if(barrier_left_flag==1)
 	{
